f1c01b90e78535998bb0b5b9af8f5e933c1a221e
Alex Thorne <alex@thorne.cc>
2017-11-18 12:48:22 +0000

    Clean code
    * fix semicolon convention
    * remove excessive whitespace
    * refactor some code into a one liner

--- src/index.js
+++ src/index.js
@@ -1,26 +1,20 @@
-import 'react-hot-loader/patch';
-import React from 'react';
-import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import App from './app.jsx';
+import 'react-hot-loader/patch'
+import React from 'react'
+import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import App from './app.jsx'
+
 
 const renderNew = Component => {
-	render(
-		<AppContainer>
-			<Component />
-		</AppContainer>,
-		document.getElementById('main')
-	);
+  render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('main')
+  )
 }
 
-renderNew(App);
+renderNew(App)
 
-if (module.hot) {
-	module.hot.accept('./app.jsx', () => {
-		const NextApp = require('./app.jsx').default;
-		renderNew(NextApp);
-	});
-}
-
-
-
+if (module.hot)
+  module.hot.accept('./app.jsx', () => renderNew(require('./app.jsx').default))
